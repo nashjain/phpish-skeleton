3812ef8783a7705fdfe20d58a076818d974eebdc
Naresh Jain <nashjain@gmail.com>
2015-01-17 21:06:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Phpish Skeleton App
+===================
+
+TechJam is a non-profit initiative to build a community of techies, who are interested in learning and exploring new technologies.
+
+More details: <http://techjam.org>
---
+++ installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# check for bash commandline options
+if [ "$1" == "create" ]; then
+
+        # make sure git is installed
+        if [ ! `which git` ]; then
+            echo "For this installer to work you'll need to install Git."
+            echo '        http://git-scm.com/'
+        fi
+
+        # clone the repository, and make sure the latest master is active
+        git clone git@github.com:nashjain/phpish-skeleton.git "./$2"
+        cd ./$2
+        rm installer.sh
+        rm -f .git
+
+        # run composer
+        php composer.phar self-update
+        php composer.phar update
+        echo 'Congratulations! The skeleton project is all setup.'
+else
+        echo 'This is not a valid installation command. Usage:'
+        echo './installer.sh create app_name'
+
+fi
